04ab05013c0c22828d81d10a72392373cf3507b6
林浩涛 <haotaolin@kugou.net>
2022-02-17 18:39:28 +0800

    0217 company

---
+++ common_val.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+import re
+import gc
+import time
+import warnings
+warnings.filterwarnings("ignore")
+
+data_path = "./data/"
+common_features_train_csv = os.path.join(data_path, "common_features_train.csv")
+common_features_test_csv = os.path.join(data_path, "common_features_test.csv")
+
+sample_skeleton_train_csv = os.path.join(data_path, "sample_skeleton_train.csv")
+sample_skeleton_test_csv = os.path.join(data_path, "sample_skeleton_test.csv")
+
+
+data_path2 = "./data/data_pro1"
+
+
+
